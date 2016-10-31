2b554ec0138d7e1b3567bceec417507510f18bb9
gdmec07150639 <954333235@qq.com>
2016-10-31 13:47:49 +0800

    Initial commit

---
+++ app/src/main/java/pr2/demo/com/intent/IntentDemo2.java
@@ -0,0 +1,28 @@
+package pr2.demo.com.intent;
+
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+/**
+ * Created by LH on 2016/10/25.
+ */
+public class IntentDemo2 extends AppCompatActivity {
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.intent_demo1);
+    }
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public void upper(View v){
+        Intent intent = getIntent();
+        Bundle bundle =intent.getExtras();
+        final String value = bundle.getString("value").toUpperCase();
+        Intent intentresult = new Intent();
+        intentresult.putExtra("result",value);
+        setResult(RESULT_OK,intentresult);
+    }
+}
---
+++ app/src/main/java/pr2/demo/com/intent/IntentDemo3.java
@@ -0,0 +1,9 @@
+package pr2.demo.com.intent;
+
+import android.app.Activity;
+
+/**
+ * Created by LH on 2016/10/25.
+ */
+public class IntentDemo3 extends Activity {
+}
